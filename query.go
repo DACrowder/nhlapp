b1f6241a73e0d1910299d3d1fc7c3c44462ecb1c
aidan <aidandbush@gmail.com>
2018-01-28 00:04:18 -0700

    Add temp function to query lines based off of shots

--- query.go
+++ query.go
@@ -8,7 +8,7 @@ func getTeamShots(gameID string, team string) (int, error) {
 	if gameID == "" || team == "" {
 		return 0, fmt.Errorf("game or team not given")
 	}
-	q := `select count (*) from event where game_id = $1 and player1_team = $2 and event_type = 'SHOT' or event_type = 'GOAL'`
+	q := `select count (*) from event where game_id = $1 and player1_team = $2 and (event_type = 'SHOT' or event_type = 'GOAL')`
 
 	row := Db.QueryRow(q, gameID, team)
 	/*if err != nil {
@@ -19,4 +19,22 @@ func getTeamShots(gameID string, team string) (int, error) {
 	row.Scan(&count)
 
 	return count, nil
+}
+
+func getLineShots(gameID string) error {
+	q := `select distinct roster.player_id, roster.event_id
+            from event_roster as roster,
+                (select event_id, player1_team as team
+                    from event
+                    where game_id = $1 and (event_type = 'SHOT' or event_type = 'GOAL')
+                ) as sub_q
+            where roster.game_id = $1 and roster.event_id = sub_q.event_id and roster.team = sub_q.team
+            order by roster.event_id asc`
+
+	_, err := Db.Query(q, gameID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
